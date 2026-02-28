3df544f63aaa7034cbdd6c47bdd186bac41dde05
JohnEff <39324199+SeJohnEff@users.noreply.github.com>
2026-02-28 22:11:12 +0100

    Initialize dialogs package with __init__.py

---
+++ dialogs/__init__.py
@@ -0,0 +1,8 @@
+"""
+Dialogs package - Modal dialogs for backup, restore, and settings.
+"""
+from dialogs.backup_dialog import BackupDialog
+from dialogs.restore_dialog import RestoreDialog
+from dialogs.settings_dialog import SettingsDialog
+
+__all__ = ['BackupDialog', 'RestoreDialog', 'SettingsDialog']
